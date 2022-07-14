4e83a647df3c7d30ee9616e35f470c2bd94c9b97
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2022-07-14 14:52:39 +0200

    Include last_modified field

--- include/setup.sql
+++ include/setup.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `publications` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(300) NOT NULL,
   `author` varchar(300) NOT NULL,
-  `date` date NOT NULL DEFAULT CURDATE(),
+  `date` date NOT NULL DEFAULT curdate(),
   `keyword` varchar(300) DEFAULT NULL,
   `abstract` varchar(5000) DEFAULT NULL,
   `path` varchar(300) NOT NULL,
@@ -12,5 +12,11 @@ CREATE TABLE IF NOT EXISTS `publications` (
   `path_url` varchar(300) DEFAULT NULL,
   `password` varchar(300) DEFAULT NULL,
   `text` MEDIUMTEXT DEFAULT NULL,
+  `last_modified` datetime NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TRIGGER IF NOT EXISTS update_modified_timestamp BEFORE UPDATE
+ON publications
+FOR EACH row
+  SET new.last_modified = CURRENT_TIMESTAMP();
