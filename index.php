ec54b86659289d452e14ba27e04e3ae93ce74b40
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2022-07-19 13:47:59 +0200

    Implement full text search

--- index.php
+++ index.php
@@ -15,22 +15,33 @@ if (isset($_GET["title"])) {
 
  $pdo = get_db();
 
- // Prepared statement
- $stmt = $pdo->prepare('SELECT * FROM publications WHERE (title LIKE :search OR keyword LIKE :search OR abstract LIKE :search) AND author LIKE :auth AND type LIKE :type AND date BETWEEN :byear AND :eyear ORDER BY date DESC, title');
+ // Prcess our inputs
+ $title      = $_GET['title'];
+ $author     = ucwords($_GET["author"]);
+ $begin      = intval($_GET["begin"]) . "-01-01";
+ $end        = intval($_GET["end"]) + 1 . "-01-01";
+ $type       = strtolower($_GET["pubtype"]);
+ $searchType = $_GET["searchtype"];
 
- $title  = $_GET['title'];
- $author = ucwords($_GET["author"]);
- $begin  = intval($_GET["begin"]) . "-01-01";
- $end    = intval($_GET["end"]) + 1 . "-01-01";
- $type   = strtolower($_GET["pubtype"]);
+ $stmt;
 
- $query = [":search" => "%$title%", ":auth" => "%$author%", "type" => $type, ":byear" => $begin, "eyear" => $end];
+ // Check if we are searching for title/keywords or the full text
+ if ($searchType == "general") {
+  $stmt  = $pdo->prepare('SELECT * FROM publications WHERE (title LIKE :search OR keyword LIKE :search OR abstract LIKE :search) AND author LIKE :auth AND type LIKE :type AND date BETWEEN :byear AND :eyear ORDER BY date DESC, title');
+  $query = [":search" => "%$title%", ":auth" => "%$author%", "type" => $type, ":byear" => $begin, "eyear" => $end];
+  $stmt->execute($query);
+ } else {
+  // Process query's whitespace to include wrongly-ocr'd characters or
+  $text = preg_replace('/\s+/', '_', $text);
 
- $stmt->execute($query);
+  $stmt  = $pdo->prepare('SELECT * FROM publications WHERE text LIKE :needle AND author LIKE :auth AND type LIKE :type AND date BETWEEN :byear AND :eyear ORDER BY date DESC, title');
+  $query = [":needle" => "%$title%", ":auth" => "%$author%", "type" => $type, ":byear" => $begin, "eyear" => $end];
+  $stmt->execute($query);
+ }
 
  $pubs = $stmt->fetchAll();
 
- echo $twig->render('index.html', ['doctypes' => DOC_TYPES, 'publications' => $pubs, 'qtitle' => $title, 'qauth' => $author, 'qstart' => $begin, 'qend' => $end, 'qtype' => $type]);
+ echo $twig->render('index.html', ['doctypes' => DOC_TYPES, 'publications' => $pubs, 'qtitle' => $title, 'qauth' => $author, 'qstart' => $begin, 'qend' => $end, 'qtype' => $type, 'stype' => $searchType]);
 } else {
  echo $twig->render('index.html', ['doctypes' => DOC_TYPES]);
 }
